38c1b00b9de37f44073a3fe20687aac64319fe19
Pavel Vanecek <corkscreewe@gmail.com>
2024-11-03 15:00:32 +0900

    Remove unused state

--- src/withGlobals.ts
+++ src/withGlobals.ts
@@ -1,39 +1,28 @@
 import type {
   Renderer,
   PartialStoryFn as StoryFunction,
-  StoryContext,
 } from "@storybook/types";
 import { useEffect, useGlobals } from "@storybook/preview-api";
 import { PARAM_KEY } from "./constants";
 
 export const withGlobals = (
   StoryFn: StoryFunction<Renderer>,
-  context: StoryContext<Renderer>
 ) => {
   const [globals] = useGlobals();
   const myAddon = globals[PARAM_KEY];
-  // Is the addon being used in the docs panel
-  const isInDocs = context.viewMode === "docs";
-  const { theme } = context.globals;
 
   useEffect(() => {
     // Execute your side effect here
     // For example, to manipulate the contents of the preview
-    const selector = isInDocs
-      ? `#anchor--${context.id} .sb-story`
-      : "#storybook-root";
+    const selector = "#storybook-root";
 
-    displayToolState(selector, {
-      myAddon,
-      isInDocs,
-      theme,
-    });
-  }, [myAddon, theme]);
+    displayToolState(selector);
+  }, [myAddon]);
 
   return StoryFn();
 };
 
-function displayToolState(selector: string, state: any) {
+function displayToolState(selector: string) {
   const rootElements = document.querySelectorAll(selector);
 
   rootElements.forEach((rootElement) => {
@@ -49,13 +38,11 @@ function displayToolState(selector: string, state: any) {
       preElement.style.setProperty("border-radius", "3px");
       preElement.style.setProperty("max-width", "600px");
       preElement.style.setProperty("overflow", "scroll");
-      rootElement.appendChild(preElement);
+      // rootElement.appendChild(preElement);
     }
 
     preElement.innerText = `This snippet is injected by the withGlobals decorator.
 It updates as the user interacts with the ⚡ or Theme tools in the toolbar above.
-
-${JSON.stringify(state, null, 2)}
 `;
   });
 }
